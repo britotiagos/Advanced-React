147f9bd4e961d2bea515b787619c57c034b980ea
Tiago Brito <brito.tiagos@gmail.com>
2018-11-09 11:20:29 +1100

    Finished Lesson 36

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,9 +1,9 @@
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
@@ -20,12 +20,24 @@ server.express.use((req, res, next) => {
   next();
 });
 
+// 2. Create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    "{id, permissions, email,name}"
+  );
+  req.user = user;
+  next();
+});
+
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL,
-    },
+      origin: process.env.FRONTEND_URL
+    }
   },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
