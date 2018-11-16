9feb88da659463fd898c5f5effa6fcd11165c12d
Tiago Brito <brito.tiagos@gmail.com>
2018-11-16 12:03:11 +1100

    Finished lesson 59 Mocking Apollo Queris

--- sick-fits/frontend/components/SingleItem.js
+++ sick-fits/frontend/components/SingleItem.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import gql from "graphql-tag";
-import { Query } from "react-apollo";
-import Error from "./ErrorMessage";
-import styled from "styled-components";
-import Head from "next/head";
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import Error from './ErrorMessage';
+import styled from 'styled-components';
+import Head from 'next/head';
 
 const SingleItemStyles = styled.div`
   max-width: 1200px;
@@ -12,7 +12,7 @@ const SingleItemStyles = styled.div`
   display: grid;
   grid-auto-columns: 1fr;
   grid-auto-flow: column;
-  min-width: 800px;
+  min-height: 800px;
   img {
     width: 100%;
     height: 100%;
@@ -23,6 +23,7 @@ const SingleItemStyles = styled.div`
     font-size: 2rem;
   }
 `;
+
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
     item(where: { id: $id }) {
@@ -33,20 +34,19 @@ const SINGLE_ITEM_QUERY = gql`
     }
   }
 `;
-
 class SingleItem extends Component {
   render() {
     return (
       <Query
         query={SINGLE_ITEM_QUERY}
         variables={{
-          id: this.props.id
+          id: this.props.id,
         }}
       >
         {({ error, loading, data }) => {
           if (error) return <Error error={error} />;
           if (loading) return <p>Loading...</p>;
-          if (!data.item) return <p>No Item Found for {this.props.id} </p>;
+          if (!data.item) return <p>No Item Found for {this.props.id}</p>;
           const item = data.item;
           return (
             <SingleItemStyles>
@@ -67,3 +67,4 @@ class SingleItem extends Component {
 }
 
 export default SingleItem;
+export { SINGLE_ITEM_QUERY };
