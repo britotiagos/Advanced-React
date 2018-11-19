f7fa26f1c7443ba8c53b6744d452f594afb2b0c9
Tiago Brito <brito.tiagos@gmail.com>
2018-11-19 11:03:46 +1100

    Missign from last commit

--- sick-fits/frontend/__tests__/SingleItem.test.js
+++ sick-fits/frontend/__tests__/SingleItem.test.js
@@ -27,7 +27,6 @@ describe('<SingleItem/>', () => {
     expect(wrapper.text()).toContain('Loading...');
     await wait();
     wrapper.update();
-    // console.log(wrapper.debug());
     expect(toJSON(wrapper.find('h2'))).toMatchSnapshot();
     expect(toJSON(wrapper.find('img'))).toMatchSnapshot();
     expect(toJSON(wrapper.find('p'))).toMatchSnapshot();
@@ -49,7 +48,6 @@ describe('<SingleItem/>', () => {
     );
     await wait();
     wrapper.update();
-    console.log(wrapper.debug());
     const item = wrapper.find('[data-test="graphql-error"]');
     expect(item.text()).toContain('Items Not Found!');
     expect(toJSON(item)).toMatchSnapshot();
